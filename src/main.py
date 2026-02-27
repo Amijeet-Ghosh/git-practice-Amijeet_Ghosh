43ce36ec7fe80baac3b3d3142bd4538a4892d563
Amigeet Ghosh <amijeetghosh.minmoy@gmail.com>
2026-02-27 23:04:05 +0600

    main.py updated

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
 from datetime import datetime
+from utils import add, subtract, multiply
 
-print(" Amijeet Ghosh")
-print(f"Today's Date: {datetime.now().strftime('%Y-%m-%d')}")
+def main():
+    print("amijeet Ghosh")
+    print(f"Today's Date: {datetime.now().strftime('%Y-%m-%d')}")
+    print(f"5 + 3 = {add(78, 45)}")
+    print(f"10 - 4 = {subtract(17, 9)}")
+    print(f"6 * 7 = {multiply(7, 13)}")
 
+if __name__ == "__main__":
+    main()
